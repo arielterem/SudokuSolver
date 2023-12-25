ec7d20ebcab996049c117b55b4a75c938be364d7
arielterem <arielterem@gmail.com>
2023-12-25 20:31:02 +0200

    Initial commit with all files and folders

---
+++ SudokuA/Board.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuA
+{
+    public class Board
+    {
+        public int[,] Bboard = new int[9, 9];
+        public int counter; //count amount of 0(numbers) in array
+        public PointerNode[] pointer = new PointerNode[81];
+
+        public void SetBoard(int[,] b)
+        {
+            Bboard = b; //insert array to array
+            int index = 0; //insert indexs to pointers array
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    if (Bboard[r, c] == 0)
+                    {
+                        SetOnePointer(index, r, c);
+                        index++;
+                    }
+                }
+            }
+            SetCounter(index); //set counter
+        }
+
+        public int SetCounter(int num)
+        {
+            counter = num;
+            return counter;
+        }
+        public int CounterAdd1()
+        {
+            counter++;
+            return counter;
+        }
+        public int GetCounter()
+        {
+            counter++;
+            return counter;
+        }
+        public void ResetPointer()
+        {
+            for(int i = 0; i < pointer.Length; i++)
+                pointer[i] = null;
+        }
+        public void SetOnePointer(int index, int r, int c)
+        {
+            PointerNode pn = new PointerNode();
+            pn.SetPointer(r, c);
+            pointer[index] = pn;
+        }
+        public PointerNode[] GetPointer()
+        {
+            return pointer;
+        }
+        public PointerNode GetPointerByIndex(int index)
+        {
+            return pointer[index];
+        }
+        
+        public void Print()
+        {
+
+            int[,] grid = { { 0, 0, 0, 0, 9, 0, 1, 0, 6 }, //REAL
+                            { 3, 1, 0, 0, 0, 5, 0, 0, 0 },
+                            { 0, 0, 5, 0, 0, 0, 0, 7, 0 },
+                            { 6, 0, 0, 0, 0, 0, 2, 4, 0 },
+                            { 0, 9, 0, 0, 7, 0, 0, 6, 8 },
+                            { 0, 3, 2, 0, 0, 0, 0, 0, 1 },
+                            { 0, 5, 0, 0, 0, 0, 4, 1, 0 },
+                            { 0, 6, 0, 5, 0, 0, 0, 8, 2 },
+                            { 4, 0, 1, 0, 8, 0, 0, 0, 0 } };
+
+            //{ { 3, 0, 6, 5, 0, 8, 4, 0, 0 }, //REAL
+            //            { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+            //            { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+            //            { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+            //            { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+            //            { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+            //            { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+            //            { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+            //            { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
+        //};
+
+            for (int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if (Bboard[i, j] != 0 )
+                    {
+                        if(grid[i, j] != 0)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(String.Format("{0,-2}", Bboard[i, j]));
+                    }
+                    else
+                        Console.Write(String.Format("{0,-2}", " "));
+                    Console.ResetColor();
+                    if ((j + 1) % 3 == 0 && j != 8)
+                        Console.Write(String.Format("{0,-2}", "| "));
+                }
+                Console.WriteLine();
+                if ((i + 1) % 3 == 0 && i != 8)
+                    Console.WriteLine("----------------------");
+            }
+        }
+
+        public void PrintPointers()
+        {
+            for(int i = 0; i < counter; i++)
+            {
+                Console.Write(i + " > " + pointer[i].row + "," + pointer[i].col + " | ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class PointerNode
+    {
+        public int row;
+        public int col;
+
+        public void SetRow(int r)
+        {
+            row = r;
+        }
+        public void SetCol(int c)
+        {
+            col = c;
+        }
+        public int GetRow()
+        {
+            return row;
+        }
+        public int GetCol()
+        {
+            return col;
+        }
+        public void SetPointer(int r, int c)
+        {
+            row = r;
+            col = c;
+        }
+        public PointerNode GetPointer()
+        {
+            PointerNode p = new PointerNode();
+            p.SetRow(GetRow());
+            p.SetCol(GetCol());
+            return p;
+        }
+    }
+
+    
+}
---
+++ SudokuA/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SudokuA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[,] grid = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 }, //REAL
+                            { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                            { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                            { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                            { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                            { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                            { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                            { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                            { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+           int[,] grid1 = { { 0, 0, 0, 0, 9, 0, 1, 0, 6 }, //REAL
+                            { 3, 1, 0, 0, 0, 5, 0, 0, 0 },
+                            { 0, 0, 5, 0, 0, 0, 0, 7, 0 },
+                            { 6, 0, 0, 0, 0, 0, 2, 4, 0 },
+                            { 0, 9, 0, 0, 7, 0, 0, 6, 8 },
+                            { 0, 3, 2, 0, 0, 0, 0, 0, 1 },
+                            { 0, 5, 0, 0, 0, 0, 4, 1, 0 },
+                            { 0, 6, 0, 5, 0, 0, 0, 8, 2 },
+                            { 4, 0, 1, 0, 8, 0, 0, 0, 0 } };
+            Board B1 = new Board();
+            B1.SetBoard(grid1);
+            B1.Print();
+            Console.WriteLine("Sudoku solution:");
+
+            if(Solve(B1) == true)
+                B1.Print();
+            else
+                Console.WriteLine("Fail");
+
+            Console.ReadKey();
+        }
+        
+        public static void SetBoardPointers(Board b)
+        {
+            int index = 0;
+            for(int r = 0; r < 9; r++)
+            {
+                for(int c = 0; c < 9; c++)
+                {
+                    if (b.Bboard[r, c] > 0)
+                    {
+                        b.SetOnePointer(index, r, c);
+                        index++;
+
+                    }
+                }
+            }
+        }
+        public static bool IfBoardValid(Board b)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if (b.Bboard[i,j] != 0)
+                    {
+                        if (IfBoardValidHelper(b, j, i) == false)
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool IfBoardValidHelper(Board b, int c, int r)
+        {
+            for (int i = 0; i < 9; i++) //scan cols
+                if (i != r)
+                    if (b.Bboard[i, c] == b.Bboard[r, c])
+                        return false;
+            for (int i = 0; i < 9; i++) //scan rows
+                if (i != c)
+                    if (b.Bboard[r, i] == b.Bboard[r, c])
+                        return false;
+
+            for (int i = c - (c % 3); i < (3 + c - (c % 3)); i++)  //scan sq
+            {
+                for (int j = r - (r % 3); j < (3 + r - (r % 3)); j++)
+                {
+                    if (i != c && j != r)
+                        if (b.Bboard[j, i] == b.Bboard[r, c])
+                            return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool Solve(Board B)
+        {
+            if(B.counter == 0)
+                return true;
+            int count = B.counter;
+            SolveA(B);
+            if (count == B.counter)
+                return false;
+            else
+                Solve(B);
+
+            return true;
+        }
+        public static bool SolveA(Board B)
+        {
+            if (B.counter == 0)
+                return true;
+            int row = B.GetPointerByIndex(B.counter - 1).row;
+            int col = B.GetPointerByIndex(B.counter - 1).col;
+            B.pointer[B.counter - 1] = null;
+            B.counter--;
+            for (int i = 1; i <= 9; i++)
+            {
+                if (B.counter >= 0)
+                {
+                    B.Bboard[row, col] = i;
+                    if (IfBoardValid(B) == true)
+                    {
+                        if (SolveA(B) == true)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                    return true;
+            }
+            B.Bboard[row, col] = 0;
+            B.counter++;
+            B.SetOnePointer(B.counter - 1, row, col);
+            return false;
+
+        }
+
+    }
+}
